93bd3561ed7351ef8d8b39f775ccfad380347704
Axiaaa <lucie.camerlynck@hotmail.com>
2026-06-29 11:30:05 -0400

    clippy ☠️

--- src/gui/views/emulation_view.rs
+++ src/gui/views/emulation_view.rs
@@ -262,6 +262,6 @@ impl From<DebuggingDevice> for SelectionDevice {
 impl From<EmulationDevice> for SelectionDevice {
     fn from(value: EmulationDevice) -> Self {
         let key_mapping = value.core_game.key_mapping.clone();
-        Self { key_mapping: key_mapping, ..Default::default() }
+        Self { key_mapping, ..Default::default() }
     }
 }
